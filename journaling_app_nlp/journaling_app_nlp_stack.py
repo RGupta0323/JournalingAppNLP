59ea49b8e327076007d797a048df12663935eb1d
RGupta0323 <guptarohan323@gmail.com>
2023-03-28 17:46:02 -0500

    adding github actions pipeline

--- journaling_app_nlp/journaling_app_nlp_stack.py
+++ journaling_app_nlp/journaling_app_nlp_stack.py
@@ -2,6 +2,10 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_lightsail as lightsail, 
+    aws_s3 as s3, 
+    aws_lambda as _lambda,
+    aws_s3_notifications as s3_notify
 )
 from constructs import Construct
 
@@ -10,10 +14,30 @@ class JournalingAppNlpStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        
+        # s3 bucket to store user journal entries 
+        journal_entries_s3_bucket = s3.Bucket(self, "JournalingNLPAppJournalEntries",
+                                               encryption=s3.BucketEncryption.KMS, bucket_name="JournalingNLPAppJournalEntries")
+        
+        # lambda to handle journal entries 
+        lambda_func = _lambda.Function(self, 'Journal_Entry_Created_Lambda',
+                       runtime=_lambda.Runtime.PYTHON_3_8,
+                       handler='Journal_Entry_Created_Lambda.handler',
+                       code=_lambda.Code.asset('src'),
+                       environment={'BUCKET_NAME':
+                                    journal_entries_s3_bucket.bucket_name})
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "JournalingAppNlpQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        
+        notification = s3_notify.LambdaDestination(lambda_func)
+        notification.bind(self, journal_entries_s3_bucket)
+
+         # Step function / eventbridge here to handle data formatting 
+
+        # Sage maker 
+        
+        # dynamodb to store user data 
+
+        # light sail instance that's going to be hosting the flask app 
+        
+
+        # Lambdas 
