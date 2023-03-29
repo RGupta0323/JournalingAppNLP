3e570697dda0c8d0b41121acf3134f5435030c82
Rohan Gupta <guptarohan323@gmail.com>
2023-03-29 08:21:00 -0500

    Fixing bugs

--- journaling_app_nlp/journaling_app_nlp_stack.py
+++ journaling_app_nlp/journaling_app_nlp_stack.py
@@ -18,8 +18,8 @@ class JournalingAppNlpStack(Stack):
         # s3 bucket to store user journal entries - this is when a user creates a journal entry. 
         # May need a folder structure in the future for after the file is modified by the lambda (Ready for sagemaker), 
         # then another flag or tag will be needed to know when sagemaker model has ran... 
-        journal_entries_s3_bucket = s3.Bucket(self, "JournalingNLPAppJournalEntries",
-                                               encryption=s3.BucketEncryption.KMS, bucket_name="JournalingNLPAppJournalEntries")
+        journal_entries_s3_bucket = s3.Bucket(self, "JournalingNLPAppJournalEntries".lower(),
+                                               encryption=s3.BucketEncryption.KMS, bucket_name="JournalingNLPAppJournalEntries".lower())
         
         # lambda to handle journal entries 
         lambda_func = _lambda.Function(self, 'Journal_Entry_Created_Lambda',
