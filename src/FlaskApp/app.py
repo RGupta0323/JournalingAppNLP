6aa6c3dd559c0466299657b997300ef92dd42f52
RGupta0323 <guptarohan323@gmail.com>
2023-04-05 08:22:23 -0500

    fixed re-direct issue

--- src/FlaskApp/app.py
+++ src/FlaskApp/app.py
@@ -1,5 +1,5 @@
 # app.py
-from flask import Flask ,render_template, request
+from flask import Flask ,render_template, request, redirect, url_for
 import boto3 
 import datetime 
 
@@ -12,43 +12,51 @@ def home(): # route handler function
 
 
 # backend routes to handle user requests (not publicly accessible urls)
-@app.route("/usersubmitsjournalentry", methods=["POST"])
+@app.route("/usersubmitsjournalentry", methods=["POST", "GET"])
 def usersubmitsjournalentry(): 
-    user_form = request.form 
-    print("[app.py usersubmitsjournalentry() line 15] user: {}".format(user_form))
-    print("immutable dict keys: {}".format(user_form.keys())) 
-    #print("[app.py usersubmitsjournalentry() line 17] journal title: {}".format(user_form.get("journal_title")))
-    journal_title, journal_data = user_form.get("journal_title"), user_form.get("journal_data")
+    if(request.method == "POST"): 
+        user_form = request.form 
+        print("[app.py usersubmitsjournalentry() line 15] user: {}".format(user_form))
+        print("immutable dict keys: {}".format(user_form.keys())) 
+        #print("[app.py usersubmitsjournalentry() line 17] journal title: {}".format(user_form.get("journal_title")))
+        journal_title, journal_data = user_form.get("journal_title"), user_form.get("journal_data")
 
-    # from here this should convert this data into a file and save that file in an s3 bucket, which should trigger the 
-    # event notification to trigger the lambda 
-    #s3_bucket = "arn:aws:s3:::journalingappnlpstack-journalingnlpappjournalentr-sx75n97n9" #arn of s3 bucket for journal entires 
-    s3_bucket = "journalingappnlpstack-journalingnlpappjournalentr-sx75n97n9"
-    client = None 
-    
+        # from here this should convert this data into a file and save that file in an s3 bucket, which should trigger the 
+        # event notification to trigger the lambda 
+        #s3_bucket = "arn:aws:s3:::journalingappnlpstack-journalingnlpappjournalentr-sx75n97n9" #arn of s3 bucket for journal entires 
+        s3_bucket = "journalingappnlpstack-journalingnlpappjournalentr-sx75n97n9"
+        client = None 
+        
 
-    try: 
-        # getting timestamp for user 
-        print("[app.py usersubmitsjouranlentry() line 32] Getting timestamp for user ")
-        ct = datetime.datetime.now() 
-        client = boto3.client("s3") 
-        print("[app.py usersubmitsjournalentry() line 26] Uploading user journal entry data into s3 bucket")
-        # (TODO) the key should be the email of the user and the timestamp; format: <email>-<timestamp>
-        client.put_object(Body=journal_data, Bucket=s3_bucket, Key="UserJournalEntry-{}.txt".format(ct)) 
+        try: 
+            # getting timestamp for user 
+            print("[app.py usersubmitsjouranlentry() line 32] Getting timestamp for user ")
+            ct = datetime.datetime.now() 
+            client = boto3.client("s3") 
+            print("[app.py usersubmitsjournalentry() line 26] Uploading user journal entry data into s3 bucket")
+            # (TODO) the key should be the email of the user and the timestamp; format: <email>-<timestamp>
+            client.put_object(Body=journal_data, Bucket=s3_bucket, Key="UserJournalEntry-{}-{}.txt".format(journal_title, ct)) 
 
-    except Exception as ex: 
-        print ("[app.py usersubmitsjournalentry() line 32] ERROR OCCURED - Exception message: {}".format(ex))
-        return ex 
-    
-    print("[app.py usersubmitsjouranlentry() line 43] Put object in s3 bucket operation successful!")
+        except Exception as ex: 
+            print ("[app.py usersubmitsjournalentry() line 32] ERROR OCCURED - Exception message: {}".format(ex))
+            return ex 
+        
+        print("[app.py usersubmitsjouranlentry() line 43] Put object in s3 bucket operation successful!")
+        print(url_for('journalentrysubmitted'))
+        return redirect(url_for('journalentrysubmitted'),code=200)
+        #return render_template("UserHome.html")
 
-    # render a webpage to thank a user for the journal entry - this is where the user will get 
-    # reccomendations about meditations to use, wisdom, articles, etc. 
+        # render a webpage to thank a user for the journal entry - this is where the user will get 
+        # reccomendations about meditations to use, wisdom, articles, etc. 
 
-    # the user can also access their workspace 
-    return render_template("JournalEntrySubmitted.html")
-    
-    
+        # the user can also access their workspace 
+        
+       
+
+
+@app.route("/journalentrysubmitted", methods=["GET", "POST"])
+def journalentrysubmitted(): 
+    return render_template("UserHome.html") 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- src/Journal_Entry_Created_Lambda.py
+++ src/Journal_Entry_Created_Lambda.py
@@ -1,3 +1,21 @@
+"""
+event = {'Records': [{'eventVersion': '2.1', 'eventSource': 'aws:s3', 'awsRegion': 'us-east-1', 
+'eventTime': '2023-04-04T19:15:02.851Z', 'eventName': 'ObjectCreated:Put', 'userIdentity': 
+{'principalId': 'AWS:AIDA3NCGCBMDJN2K54XTQ'}, 'requestParameters': {'sourceIPAddress': '173.215.110.3'},
+ 'responseElements': {'x-amz-request-id': 'PQ4FZNQ0Q2R646PV', 
+ 'x-amz-id-2': 'kjzTO0yjBR3zsT05bXfUxkV3Bcgkfyvnc2UVf1BEGhHH039QZTjOnw1t4LWh0lON5prhAJjzvstp3Dc6jCxKddQZn1U84JG2'}, 
+ 's3': {'s3SchemaVersion': '1.0', 'configurationId': 'ZmQxMDY5NDMtN2NjNS00NjRmLWI3ZjEtMGQxNjUwODg3NDEx', 
+ 'bucket': {'name': 'journalingappnlpstack-journalingnlpappjournalentr-sx75n97n9',
+   'ownerIdentity': {'principalId': 'AR223LMC2GQLQ'}, 
+   'arn': 'arn:aws:s3:::journalingappnlpstack-journalingnlpappjournalentr-sx75n97n9'}, 
+   'object': {'key': 'UserJournalEntry-2023-04-04+14%3A15%3A01.673145.txt', 'size': 23, 
+   'eTag': '96e29a6011b52746e0ba1ae100988610', 'sequencer': '00642C7736A2D29B08'}}}]}
+"""
+
+# this lambda is going to take the input event (see above) and it will open up the file that was just uploaded 
+# from there its going to take the data and section it out to list of lists (for now)
+# then it will put this data into s3 or directly input it into sagemaker 
+# from there the ml model will be triggered. 
 def handler(event, context): 
     print("[Journal_Entry_Created_Lambda.py handler() line 2] event: {}".format(event)) 
 
